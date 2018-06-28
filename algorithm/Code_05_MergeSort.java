6569cab5cae9e4c17133878bde5b44481bae2953
杭京伟 <39057197+Smarthangjingwei@users.noreply.github.com>
2018-06-28 18:43:38 +0800

    Update Code_05_MergeSort.java
    加入部分注释帮助理解。

--- algorithm/Code_05_MergeSort.java
+++ algorithm/Code_05_MergeSort.java
@@ -30,8 +30,8 @@ public class Code_05_MergeSort {
 		int i = 0;
 		int p1 = l;
 		int p2 = m + 1;
-		while (p1 <= m && p2 <= r) {
-			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+		while (p1 <= m && p2 <= r) {//p1,p2两个指针，p1从l位置开始到m,p2从m+1位置到r。
+			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];//p1和p2指向的谁更小就存入help数组。
 		}
 		while (p1 <= m) {
 			help[i++] = arr[p1++];
